81ea7c1285e1a0ca61c79f035b3af01bff14ab11
zhaitao <370985116@qq.com>
2015-10-14 11:43:56 +0800

    C代码

--- app/src/main/java/io/serniorzhao/as_ndk/MainActivity.java
+++ app/src/main/java/io/serniorzhao/as_ndk/MainActivity.java
@@ -1,13 +1,22 @@
 package io.serniorzhao.as_ndk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("hello-jni");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.text);
+        tv.setText(testLog());
     }
+
+    public static native String testLog();
 }
